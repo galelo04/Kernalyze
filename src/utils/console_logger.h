188e9348e7fb8c08758db990407e17f62f8251ef
AhmedAmrNabil <ahmedamr2480@gmail.com>
2025-04-23 22:43:50 +0200

    feat: add helper functions for logging

---
+++ src/utils/console_logger.h
@@ -0,0 +1,46 @@
+#ifndef CONSOLE_LOGGER_H
+#define CONSOLE_LOGGER_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include "colors.h"
+
+void printInfo(const char* module, const char* format, ...) {
+    printf("[" ANSI_COLOR_BOLD_BLUE "%s" ANSI_COLOR_RESET "] ", module);
+
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+
+    printf("\n");
+    fflush(stdout);
+}
+
+void printError(const char* module, const char* format, ...) {
+    printf("[" ANSI_COLOR_BOLD_RED "%s" ANSI_COLOR_RESET "] ", module);
+
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+
+    printf("\n");
+    fflush(stdout);
+}
+
+void printWarning(const char* module, const char* format, ...) {
+    printf("[" ANSI_COLOR_BOLD_YELLOW "%s" ANSI_COLOR_RESET "] ", module);
+
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+
+    printf("\n");
+    fflush(stdout);
+}
+
+#endif  // CONSOLE_LOGGER_H
