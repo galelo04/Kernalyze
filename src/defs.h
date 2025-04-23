9077bac950775c99546cbfc80c7707b7a1b870e9
Mostafa Ahmed Abdelglel <139139781+galelo04@users.noreply.github.com>
2025-04-23 21:37:29 +0200

    feat: PCB structure (#3)
    * chore: initialize project with copied boilerplate files
    copied from https://github.com/cmp-2060-sp25/kernel-sim-kernalyze
    
    * feat: added PCB structure
    
    * feat: added list implementation to store PCBs

---
+++ src/defs.h
@@ -0,0 +1,13 @@
+#ifndef DEFS_H
+#define DEFS_H
+#include <sys/types.h>
+
+typedef enum {READY, RUNNING, FINISHED} PROCESS_STATE;
+struct PCB{
+    pid_t pid;
+    int id ,priority , arriveTime ,remainigTime, waitTime , executionTime , startTime , finishTime;
+    double turnaroundTime , weightedTurnaroundTime;
+    PROCESS_STATE state; 
+};
+
+#endif
---
+++ src/utils.h
@@ -0,0 +1,26 @@
+#ifndef UTILS_H
+#define UTILS_H
+#include "defs.h"
+
+//linked list node and list definitions for stroing the pcb_table
+struct NodePCB{
+    struct PCB data;
+    struct NodePCB* next;
+};
+
+struct ListPCB
+{
+    struct NodePCB* head;
+    int size;
+};
+
+struct NodePCB * createNode(struct PCB data);
+struct ListPCB * createList();
+void insertAtFront(struct ListPCB * list , struct PCB data);
+void freeList(struct ListPCB * list);
+struct PCB * findPCB(struct ListPCB * list , pid_t pid);
+
+
+
+
+#endif
