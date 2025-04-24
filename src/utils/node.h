b6b248df11e5eec2448ea9916ef2f08e17582c93
Mostafa Ahmed Abdelglel <139139781+galelo04@users.noreply.github.com>
2025-04-24 04:11:52 +0200

    Data structures (#6)
    * refactor: refactor node and list to be generic using void pointers
    
    * feat: added circular queue data sturcture
    
    * hotfix: renamed enQueue to enqueue
    
    * fix: move createNode to node.h
    
    * feat: update circular queue implementation
    
    ---------
    
    Co-authored-by: AhmedAmrNabil <ahmedamr2480@gmail.com>

---
+++ src/utils/node.h
@@ -0,0 +1,22 @@
+#ifndef NODE_H
+#define NODE_H
+
+#include <stddef.h>
+
+struct Node {
+    void * data;
+    struct Node* next;
+};
+
+struct Node* createNode(void *data){
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (!newNode) {
+        perror("malloc: ");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode; 
+}
+
+#endif
