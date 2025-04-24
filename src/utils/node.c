a2c5fdd670a4b91aba3825de96ecfbfcbfae9450
Ahmed Sobhy <samahmedsobhy@gmail.com>
2025-04-24 12:01:17 +0200

    feat: implement createNode function in node.c and update node.h

---
+++ src/utils/node.c
@@ -0,0 +1,12 @@
+#include "node.h"
+
+struct Node *createNode(void *data) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (!newNode) {
+        perror("malloc: ");
+        exit(1);
+    }
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
