0532107801e4379849f4aec5731fc5b4df8f0fb1
MoBahgat010 <mbahgat503@gmail.com>
2025-04-24 15:04:49 +0200

    process v0

--- src/process.c
+++ src/process.c
@@ -1,15 +1,59 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
 #include "clk.h"
 
-/* Modify this file as needed*/
-
 void run_process(int runtime) {
+    if (runtime <= 0) {
+        fprintf(stderr, "Process %d: Invalid runtime: %d\n", getpid(), runtime);
+        return;
+    }
+
     sync_clk();
 
-    // placeholder
-    while (runtime) {
-        runtime--;
+    printf("Process %d: Starting CPU-bound work for %d seconds...\n", getpid(), runtime);
+
+    int remaining_time = runtime;
+    int last_clock = get_clk();
+
+    while (remaining_time > 0) {
+        // Wait for the next clock tick
+        while (get_clk() == last_clock) {
+            usleep(1000);
+        }
+
+        last_clock = get_clk();
+        remaining_time--;
+
+        printf("Process %d: Executed for 1 second, %d seconds remaining at time %d.\n", 
+               getpid(), remaining_time, last_clock);
     }
-    // TODO: Keep running the process till its runtime is over
+
+    printf("Process %d: Finished CPU-bound work.\n", getpid());
+
+    pid_t parent_pgid = getpgid(getppid());
+    if (parent_pgid == -1) {
+        perror("Process: Failed to get parent process group ID");
+    } else {
+        if (kill(-parent_pgid, SIGUSR1) == -1) {
+            perror("Process: Failed to send SIGUSR1 to parent process group");
+        } else {
+            printf("Process %d: Sent SIGUSR1 to parent process group %d at time %d.\n", 
+                   getpid(), parent_pgid, get_clk());
+        }
+    }
 
     destroy_clk(0);
+}
+
+//main I do not know if we will need it but here it is
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <runtime>\n", argv[0]);
+        return 1;
+    }
+    int runtime = atoi(argv[1]);
+    run_process(runtime);
+    return 0;
 }
