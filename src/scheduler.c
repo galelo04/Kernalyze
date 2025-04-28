3894178e96271aa24f0e3ae852d1dbbfafd217e2
Mostafa Abdelglel <14712022100624@stud.cu.edu.eg>
2025-04-28 23:18:12 +0300

    hotfix: fix priority in min heap

--- src/scheduler.c
+++ src/scheduler.c
@@ -357,11 +357,11 @@ void pushToReadyQueue(struct PCB *pcb) {
     } else if (schedulerType == 1) {
         // SJF
         struct Heap *SJFreadyQueue = (struct Heap *)readyQueue;
-        heap_insert(SJFreadyQueue, (void *)pcb, *pcb->remainingTime);
+        heap_insert(SJFreadyQueue, (void *)pcb, (*pcb->remainingTime)*10000+pcb->id);
     } else if (schedulerType == 2) {
         // Priority
         struct Heap *HPFreadyQueue = (struct Heap *)readyQueue;
-        heap_insert(HPFreadyQueue, (void *)pcb, pcb->priority);
+        heap_insert(HPFreadyQueue, (void *)pcb, (pcb->priority)*10000 + pcb->id);
     }
 }
 
